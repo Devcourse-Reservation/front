91211c6be19b9c539da2643475c5c456c308890b
ShJang32 <jangseohie32@gmail.com>
2025-01-20 21:56:09 +0900

    Revert "메인페이지 css 완성"

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
-import React from "react";
-import "./App.css";
-import Layout from "./components/layouts/Layout";
-import ReservationPart from "./components/ReservationPart";
-import BackgroundBox from "./components/images/Background";
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 
-const App: React.FC = () => {
+function App() {
   return (
-    <Layout>
-      <BackgroundBox>
-        <ReservationPart />
-      </BackgroundBox>
-    </Layout>
-  );
-};
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  )
+}
 
-export default App;
+export default App
--- src/components/layouts/Footer.tsx
+++
@@ -1,8 +0,0 @@
-const Footer = () => {
-    return (
-        <div className="footer-container" style={{ height: "250px" }}>
-            <footer>푸터 컴포넌트 입니다.</footer>
-        </div>
-    )
-}
-export default Footer
