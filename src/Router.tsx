7f2d5ae0d55340f3f4c589b7ee87a9c1b0c9cdd0
ShJang32 <jangseohie32@gmail.com>
2025-01-22 09:52:50 +0900

    Revert "Revert "Revert 4 feature/mainpage""

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import App from "./App"
+import Main from "./pages/main";
 
 const Router = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<App />} />
+                <Route path="/main" element={<Main />} />
             </Routes>
         </BrowserRouter>
     );
