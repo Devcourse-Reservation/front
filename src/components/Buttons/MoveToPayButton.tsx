766d32f1cc6d15057bd24f4490d2ee6f57b500d9
jangseoheehufs <wkdtjgml0302@naver.com>
2025-02-03 01:56:45 +0900

    도착 항공편 리스트 페이지에서 다음 버튼 추가

---
+++ src/components/Buttons/MoveToPayButton.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react'
+import Button from '@mui/material/Button'
+import Stack from '@mui/material/Stack'
+import { useNavigate } from 'react-router-dom'
+
+export default function MoveToPayButton() {
+  const navigate = useNavigate()
+
+  const handleSearch = () => {
+    navigate('/arrival-list')
+  }
+
+  return (
+    <Stack direction="row" spacing={2}>
+      <Button
+        variant="contained"
+        size="large"
+        onClick={handleSearch}
+        sx={{
+          fontSize: 20,
+          borderRadius: '15px',
+          backgroundColor: '#1E2A3C',
+        }}
+      >
+        다음
+      </Button>
+    </Stack>
+  )
+}
