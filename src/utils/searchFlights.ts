e4b2e456bad6f245a97481d4928ba7014a76f920
jangseoheehufs <wkdtjgml0302@naver.com>
2025-02-03 02:02:53 +0900

    홈에서 입력 받은 데이터를 항공편 검색 페이지에서 조회할 수 있도록 하는 중입니다.

---
+++ src/utils/searchFlights.ts
@@ -0,0 +1,42 @@
+export const searchFlights = async ({
+  origin,
+  destination,
+  departureDate,
+  returnDate,
+  passengers,
+  seatClass,
+}: {
+  origin: string
+  destination: string
+  departureDate: string | null
+  returnDate: string | null
+  passengers: number
+  seatClass: string
+}) => {
+  try {
+    const response = await fetch('url', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        origin: origin.split(' ')[0], //공항 코드만 추출
+        destination: destination.split(' ')[0],
+        departureDate,
+        returnDate,
+        passengers,
+        seatClass,
+      }),
+    })
+
+    if (!response.ok) {
+      throw new Error('검색 요청 실패')
+    }
+
+    const data = await response.json()
+    console.log('검색 결과: ', data)
+
+    return data
+  } catch (err) {
+    console.error('항공편 검색 중 오류 발생: ', err)
+    throw err
+  }
+}
